b4b3e8bd1785db96dcbe0097414a63ea8db94630
sruagrawal <sagrawa8@uncc.edu>
2018-07-30 11:54:13 -0400

    Comments

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 
 
-## Description
+## Description / How to Play
 
 This is a card matching game created for the Udacity Front End Web Development Nanodegree.
-Players are given 16 cards, 8 pairs, and have to guess where the pairs are. Score is tracked via number of moves and a star system.
+Players are given 16 cards, 8 pairs, and have to guess where the pairs are by clicking on the cards. Score is tracked via number of moves and a star system.
 The game also records the amount of time it took players to complete. 
 
 Most of the HTML and CSS file was provided by Udacity.
+
+## Dependencies
+
+This project uses font-awesome.
--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@
  */
 var cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
 
+/**
+ *  movNum - Number of moves the player uses, deck - holds all cards, openList - card that is open and shown
+ *  counting - if the game has started (used for time), time - number of seconds the game has gone on for
+ */
 var movNum, deck, openList, counting, time;
-
+/**
+ * Variables that reference elements in html file
+ */
 var moves = document.getElementsByClassName('moves')[0];
 
 var stars = document.getElementsByClassName('fa-star');
@@ -19,11 +25,16 @@ var gameOverMod = document.getElementById('endGame');
 
 var repeatGameEnd = document.getElementById('repeatGame');
 
+//If player restarts game
 repeatGameEnd.addEventListener('click', () => newGame());
 repeatButton.addEventListener('click', () => newGame());
+
+//initiallizes game
 newGame();
 
-
+/**
+ * @description Shuffles cards, creates thier HTML, and displays them on the page
+ */
 function newDeck(){
   cards = shuffle(cards);
   deck = document.getElementsByClassName('deck')[0];
@@ -55,6 +66,10 @@ function shuffle(array) {
     return array;
 }
 
+/**
+ * @description Adds an Event Listener to each card. On click, card will start the game if it hasn't been started,
+ * if the card is not matched yet or already open, it will show it and consider it for match
+ */
 function cardTarget(){
   deck = deck.children;
 
@@ -65,7 +80,7 @@ function cardTarget(){
         counting = true;
         countSeconds();
       }
-      if(e.target.className != 'card match' && e.target != openList){
+      if(e.target.className != 'card match' && e.target.className != 'open show' && e.target != openList){
         show(e.target);
         open(e.target);
       }
@@ -73,11 +88,18 @@ function cardTarget(){
   }
 }
 
+/**
+ * @description Shows the cards face to the player
+ */
 function show(card)
 {
   card.className += ' open show';
 }
 
+/**
+ * @description If it is the first card opened, it assigns it to openList. If it
+ * the second card open, consider if it is a match. Increment the number of moves.
+ */
 function open(card)
 {
   if(openList == null)
@@ -92,27 +114,42 @@ function open(card)
   }
 }
 
+/**
+ * @description Cards are not a match. Briefly show that they are wrong and then hide them.
+ */
 function wrong(card){
   card.className += ' wrong';
   openList.className += ' wrong'
   setTimeout(() => hide(card), 300);
 }
 
+/**
+ * @description Hide the card from the player. Set openList back to null so that
+ * it can be reassigned once a new first card is picked.
+ */
 function hide(card){
   openList.className = 'card';
   openList = null;
   card.className = 'card';
 }
 
+/**
+ * @description If the cards are a match, change thier class to match, reset openList.
+ * Consider if the end game goal has been met.
+ */
 function match(card){
   openList.className = 'card match';
   card.className = 'card match';
   openList = null;
   matchedCards = document.getElementsByClassName('match').length;
-  if(matchedCards == 16)
+  if(matchedCards == 16) //if game is over
     setTimeout(() => gameOver(), 500);
 }
 
+/**
+ * @description Increases the number of moves the player has used and displays
+ * to page. Also in charge of number of stars player receives.
+ */
 function incMoves(){
   movNum++;
   moves.innerHTML = movNum;
@@ -122,6 +159,10 @@ function incMoves(){
   }
 }
 
+/**
+ * @description Sets all variables back to default values and sets up a new game. If game over
+ * modal is visible, hides it.
+ */
 function newGame(){
   counting = false;
   time = 0;
@@ -135,6 +176,9 @@ function newGame(){
   gameOverMod.style = "display: none";
 }
 
+/**
+ * @description Resets the number of stars on the page.
+ */
 function resetStars(){
   usedStars = document.getElementsByClassName('fa fa-star-o');
   while(usedStars[0])
@@ -143,6 +187,10 @@ function resetStars(){
   }
 }
 
+/**
+ * @description Once the game over goal is met, timer needs to stop counting
+ * and modal needs to be displayed with accurate final number of moves, stars and seconds.
+ */
 function gameOver(){
   counting = false;
   gameOverMod.style = "display: inline";
@@ -151,9 +199,12 @@ function gameOver(){
   var finStars = document.getElementById('finalStars');
   finStars.innerHTML = stars.length;
   var finSec = document.getElementById('finalSeconds');
-  finSec.innerHTML = time+1;
+  finSec.innerHTML = time+1; //Adding 1 for bug fix
 }
 
+/**
+ * @description Timer for how long the game lasted. Displays current time used on page.
+ */
 function countSeconds(){
   time += 1;
   seconds.innerHTML = time;
