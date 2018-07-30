b917d7b0505b33f3cbac488bbb06caa45b58005a
sruagrawal <sagrawa8@uncc.edu>
2018-07-29 22:29:16 -0400

    Code Clean-Up

--- js/app.js
+++ js/app.js
@@ -22,12 +22,7 @@ var repeatGameEnd = document.getElementById('repeatGame');
 repeatGameEnd.addEventListener('click', () => newGame());
 repeatButton.addEventListener('click', () => newGame());
 newGame();
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
 
 function newDeck(){
   cards = shuffle(cards);
@@ -57,7 +52,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -78,16 +72,6 @@ function cardTarget(){
     });
   }
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 function show(card)
 {
